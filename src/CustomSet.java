e693d6b4b733d866053bd7c695f0bd9b8fe4fdd3
happypoot300 <happypoot300@gmail.com>
2023-12-14 12:48:11 +0800

    added nilberto commints to CustomSet Class, testing nalang and error cheking if meron pa

--- src/CustomSet.java
+++ src/CustomSet.java
@@ -30,58 +30,68 @@ public class CustomSet<Type> {
 
 	// ==========SETS OPERATIONS==========//
 
-	// add method
+	/**
+	 * The Add method, Adds an element to the set after checking if it's already
+	 * present. If the element is found, the provided prompt message is printed, and
+	 * the method returns without adding the element again. Otherwise, the element
+	 * is added to the set.
+	 *
+	 */
 	public <T> void Add(T key, String prompt) {
-		if (Contains(key)) {// check if key is available in set
+		if (Contains(key)) {
 			System.out.print(prompt);
-			return;// return if it is
+			return;
 		} // end if
 
-		CheckArrayCapacity();// calls method to expand object array
-		// if key is not in set yet, add it to elements object array
+		CheckArrayCapacity();
 		elements[size++] = key;
 	}// end method
 
 	// overload for intersection method
 	/*
-	 * basically a flipped add, where instead of skipping when a key is found, it
-	 * adds it instead
+	 * This method performs an add operation in the context of the Intersection
+	 * method. It adds the specified key to the passed on set (A or B set) only if
+	 * the key is found in in the pass on set (A or B set); otherwise, it prints the
+	 * specified prompt.
 	 */
-	public <T> void Add(String prompt, T key, CustomSet<T> setTemp) {
-		// this is the overload contains that needs the setsTemp elements and size
-		if (Contains(key, setTemp.getElements(), setTemp.getSize())) {// if key is found in set
-			CheckArrayCapacity();// handles the expanding of array
-			elements[size++] = key;// add key to elements
+	public <T> void Add(String prompt, T key, CustomSet<T> set) {
+		if (Contains(key, set.getElements(), set.getSize())) {
+			CheckArrayCapacity();
+			elements[size++] = key;
 			return;
 		} // end if
-			// if not found, print the prompt string
 		System.out.print(prompt);
 	}// end method
 
 	// overload for difference
 	/*
-	 * this add method is like the non overload add but this needs the tempSet as
-	 * parameter to compare to
+	 * This add method is for the Difference operation. It checks if the key is
+	 * present in set (A or B set), and if found, prints the provided prompt. If the
+	 * key is not found, it adds the key to the current set.
 	 */
-	public <T> void Add(String prompt, CustomSet<T> setTemp, T key) {
-		// overload of contains
-		if (Contains(key, setTemp.getElements(), setTemp.getSize())) {// if key is found
+	public <T> void Add(String prompt, CustomSet<T> set, T key) {
+		// overload of Contains
+		if (Contains(key, set.getElements(), set.getSize())) {
 			System.out.print(prompt);
-			return;// return if found
+			return;
 		} // end if
 
-		CheckArrayCapacity();// handles the expanding of array
-		elements[size++] = key;// add key to elements
+		CheckArrayCapacity();
+		elements[size++] = key;
 	}// end method
 
-	// remove
+	/*
+	 * The Remove method removes the specified key from the set. It iterates through
+	 * the elements array to find the key. If the key is found, it sets the
+	 * elementFound to true and then it calls the DeleteElement method, that takes
+	 * in the current index. from there, it delete that index and moves all the
+	 * right side elements to the left. If the key is not found, it prints a notice
+	 * indicating that the element was not found in the set.
+	 */
 	public void Remove(Type key) {
 		boolean elementFound = false;
 		for (int index = 0; index < size; index++) {
-			System.out.println("key: " + key);
-			System.out.println("Index: " + index + " element: " + elements[index]);
 			if (key.equals(elements[index])) {
-				System.out.println("It found it");
 				elementFound = true;
 				DeleteElement(index);
 				break;
@@ -100,12 +110,18 @@ public class CustomSet<Type> {
 	}// end method
 
 	// custom method
+	/*
+	 * The DeleteElement, Deletes an element from the array at the specified index.
+	 * If the last element is to be deleted, It first check if the left of it is
+	 * null and if that index is equal to the elements length. If it is, the program
+	 * sets the last element to null. If any other element is to be deleted, it
+	 * shifts elements from right to left to fill the gap.
+	 */
 	public void DeleteElement(int indexOutSide) {
-		// if the last element in the array is to be deleted
-		if (indexOutSide + 1 == elements.length) {// check if index is out of bound
-			elements[indexOutSide] = null;// make last element null
-			size--;// decrease size
-			return;// terminate method
+		if (indexOutSide + 1 == elements.length) {
+			elements[indexOutSide] = null;
+			size--;
+			return;
 		} // end if
 
 		/*
@@ -113,13 +129,17 @@ public class CustomSet<Type> {
 		 * the element that would get deleted
 		 */
 		for (int index = indexOutSide; index < size; index++) {
-			elements[index] = elements[index + 1];// move the elements of right side to left
+			elements[index] = elements[index + 1];
 		} // end if
-		size--;// decrease size
+		size--;
 	}// end method
 
 	// Union
-	// everything from A and B merged and put it to C. With no duplicates
+	/*
+	 * This method performs the union operation on two sets A and B, merging all
+	 * elements without duplicates into a new set C. The method uses a CustomSet to
+	 * represent the resulting set.
+	 */
 	public <T> CustomSet<T> Union(T[] A, T[] B, int sizeOfA, int sizeOfB) {
 		CustomSet<T> setC = new CustomSet<>();// create the setC
 
@@ -135,7 +155,10 @@ public class CustomSet<Type> {
 	}// end method
 
 	// Intersection
-	// elements that are both in A and B are put to set C
+	/*
+	 * Performs the intersection operation on sets A and B to create set C. Common
+	 * elements present in both sets A and B are added to set C.
+	 */
 	public <T> CustomSet<T> Intersection(T[] A, T[] B, int sizeOfA, int sizeOfB) {
 		CustomSet<T> tempSet = new CustomSet<>();// create tempSet to compare from
 		CustomSet<T> setC = new CustomSet<>();// this is where the final results go
@@ -153,7 +176,11 @@ public class CustomSet<Type> {
 	}// end method
 
 	// Difference
-	// all elements in A not in B are put to set C
+
+	/*
+	 * Performs the set difference operation on sets A and B to create set C.
+	 * Elements in set A that are not present in set B are added to set C.
+	 */
 	public <T> CustomSet<T> Difference(T[] A, T[] B, int sizeOfA, int sizeOfB) {
 		CustomSet<T> tempSet = new CustomSet<>();// create tempSet to compare from
 		CustomSet<T> setC = new CustomSet<>();// this is where the final results go
@@ -172,23 +199,31 @@ public class CustomSet<Type> {
 	// ==========OTHER METHODS USED==========//
 
 	// contains method
-	public <T> boolean Contains(T key) {// check if key is available in set
-		for (int index = 0; index < size; index++) {// iterate trough the elements array
-			if (key == elements[index]) {// if key is found
-				return true;// return true
+	/*
+	 * contains method Checks if the specified key is present in the elements array.
+	 * It iterates through the elements array to check if the key is present. Check
+	 * if the current element is equal to the provided key. If key the is found,
+	 * return true and If key was not found in the loop, return false.
+	 */
+	public <T> boolean Contains(T key) {
+		for (int index = 0; index < size; index++) {
+			if (key == elements[index]) {
+				return true;
 			} // end if
 		} // end for
 
-		return false;// if key was not found, return false
+		return false;
 	}// end method
 
-	/*
-	 * new parameter, the method needs to know the size of set that is using this
-	 * method.
-	 */
 	// overload contains for intersection, difference.
+	/*
+	 * Contains overloaded method to check for the presence of an element in a
+	 * specified set. The size of the set needs to be provided to perform the check.
+	 * It iterates through the set to check for the presence of the specified
+	 * element. If the element is found, return true otherwise, it returns false
+	 */
 	public <T> boolean Contains(T key, T[] set, int sizeOfSet) {
-		for (int index = 0; index < sizeOfSet; index++) {// iterate trough the elements array
+		for (int index = 0; index < sizeOfSet; index++) {
 			if (key == set[index]) {
 				return true;
 			} // end if
@@ -197,18 +232,30 @@ public class CustomSet<Type> {
 	}// end method
 
 	// checks and expands the array
+	/*
+	 * This method checks if the array is full. If it is, it expands the array by
+	 * creating a temporary array with double the size, copying existing elements to
+	 * the temporary array, and then assigning the expanded array as the new
+	 * elements array.
+	 */
 	public void CheckArrayCapacity() {
-		if (elements.length == size) {// if array is full
-			Object[] tempArray = new Object[size * 2];// expand it
+		if (elements.length == size) {
+			Object[] tempArray = new Object[size * 2];
 
-			for (int index = 0; index < size; index++) {// iterate to the array
-				tempArray[index] = elements[index];// put the elements on the temporary expanded array
+			for (int index = 0; index < size; index++) {
+				tempArray[index] = elements[index];
 			} // end for
-			elements = tempArray;// make the expanded array be the new elements
+			elements = tempArray;
 		} // end if
 	}// end method
 
 	// displays the set elements
+	/*
+	 * Displays the elements of the set in a formatted manner. The displayed format
+	 * includes a section header and each element enclosed in square brackets.
+	 * Separate elements in a row using arrows, creating a visual representation of
+	 * the set.
+	 */
 	public void Display(String set) {
 		//@formatter:off
 			System.out.print("\n" +
